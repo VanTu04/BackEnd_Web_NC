0869d9df0d43734ba206b774b076da21b659a974
Ml Anhem <m0345194302@gmail.com>
2025-03-09 09:02:37 +0700

    MergeWithTu

--- src/main/java/com/vawndev/spring_boot_readnovel/Repositories/StoryRepository.java
+++ src/main/java/com/vawndev/spring_boot_readnovel/Repositories/StoryRepository.java
@@ -1,19 +1,18 @@
 package com.vawndev.spring_boot_readnovel.Repositories;
 
-import java.util.Optional;
-
+import com.vawndev.spring_boot_readnovel.Entities.Story;
+import com.vawndev.spring_boot_readnovel.Entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.vawndev.spring_boot_readnovel.Entities.Story;
-import com.vawndev.spring_boot_readnovel.Entities.User;
+import java.util.Optional;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, String> {
-    Page<Story> findAllByStateAndIsApprovedAndIsAvailableAndDeleteAtIsNull(double rate,boolean isApproved, boolean isAvailable ,Pageable pageable);
+    Page<Story> findAllByStateAndIsApprovedAndIsAvailableAndDeleteAtIsNull(StoryState state,boolean isApproved, boolean isAvailable ,Pageable pageable);
     Optional<Story> findByIdAndAuthor(String id, User author);
     Page<Story> findAll(Pageable pageable);
-    Page<Story> findAllByStateAndIsApprovedAndIsAvailableAndDeleteAtIsNotNull(double rate,boolean isApproved, boolean isAvailable,Pageable pageable);
+    Page<Story> findAllByStateAndIsApprovedAndIsAvailableAndDeleteAtIsNotNull(StoryState state,boolean isApproved, boolean isAvailable,Pageable pageable);
 }
