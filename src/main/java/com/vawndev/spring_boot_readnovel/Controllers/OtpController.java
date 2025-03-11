24cb5f701616ae788572d06f4f48cd18a7877f76
Ml Anhem <m0345194302@gmail.com>
2025-03-11 20:18:33 +0700

    Sửa đường dẫn otp và thêm debug vào yaml

--- src/main/java/com/vawndev/spring_boot_readnovel/Controllers/OtpController.java
+++ src/main/java/com/vawndev/spring_boot_readnovel/Controllers/OtpController.java
@@ -1,6 +1,7 @@
 package com.vawndev.spring_boot_readnovel.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import com.vawndev.spring_boot_readnovel.Services.EmailService;
 import com.vawndev.spring_boot_readnovel.Services.OtpService;
 
 @RestController
-@RequestMapping("/api/otp")
+@RequestMapping("/otp")
 public class OtpController {
 
     @Autowired
@@ -18,6 +19,11 @@ public class OtpController {
 
     @Autowired
     private OtpService otpService;
+
+    @GetMapping("/test")
+    public String testEndpoint() {
+        return "Test endpoint is working!";
+    }
 
     @PostMapping("/send")
     public String sendOtp(@RequestParam String email) {
