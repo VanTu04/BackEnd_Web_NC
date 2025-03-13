0f8366b073cc31fdae96de4f4f2d2f675e69f9f8
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2025-03-14 00:29:07 +0700

    13/3 am

--- src/main/java/com/vawndev/spring_boot_readnovel/Repositories/StoryRepository.java
+++ src/main/java/com/vawndev/spring_boot_readnovel/Repositories/StoryRepository.java
@@ -5,6 +5,8 @@ import com.vawndev.spring_boot_readnovel.Entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,4 +17,11 @@ public interface StoryRepository extends JpaRepository<Story, String> {
     Optional<Story> findByIdAndAuthor(String id, User author);
     Page<Story> findAll(Pageable pageable);
     Page<Story> findAllByIsApprovedAndIsAvailableAndDeleteAtIsNotNull(boolean isApproved, boolean isAvailable,Pageable pageable);
+
+
+    @Query("select count(*) from Story s where s.isApproved = true and month(s.updatedAt) = :month and year(s.updatedAt) = :year")
+    Long countApprovedStories(@Param("month") int month, @Param("year") int year);
+
+    @Query("SELECT COUNT(s) FROM Story s WHERE s.isApproved = false AND MONTH(s.createdAt) = :month AND YEAR(s.createdAt) = :year")
+    Long countRejectedStories(@Param("month") int month, @Param("year") int year);
 }
