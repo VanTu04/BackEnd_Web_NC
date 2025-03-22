92d707ab206d7a27668e3db8b74e2e6beda0e5bb
VanTu04 <141947683+VanTu04@users.noreply.github.com>
2025-03-23 01:04:16 +0700

    21/3 pm

---
+++ src/main/java/com/vawndev/spring_boot_readnovel/Entities/Image.java
@@ -0,0 +1,22 @@
+package com.vawndev.spring_boot_readnovel.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "images")
+public class Image extends BaseEntity{
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "chapter_id", nullable = false)
+    private Chapter chapter;
+}
