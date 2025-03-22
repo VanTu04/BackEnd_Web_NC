1068426796ab60876ae5f87b806721a9a80ce6e5
Ml Anhem <m0345194302@gmail.com>
2025-03-22 20:24:31 +0700

    Update GlobalExceptionHandler.java

--- src/main/java/com/vawndev/spring_boot_readnovel/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/vawndev/spring_boot_readnovel/Exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,9 @@
 package com.vawndev.spring_boot_readnovel.Exceptions;
 
 import java.util.Map;
-import java.util.Objects;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.vawndev.spring_boot_readnovel.Dto.Responses.ApiResponse;
-
-import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
@@ -21,113 +12,7 @@ public class GlobalExceptionHandler {
 
     private static final String MIN_ATTRIBUTE = "min";
 
-    @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
-        log.error("Exception: ", exception);
-        ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
-        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
-
-        return ResponseEntity.badRequest().body(apiResponse);
-    }
-
-    @ExceptionHandler(value = AppException.class)
-    ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
-        ErrorCode errorCode = exception.getErrorCode();
-        ApiResponse apiResponse = new ApiResponse();
-
-        apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
-
-        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
-    }
-
-    @ExceptionHandler(value = org.springframework.security.access.AccessDeniedException.class)
-    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
-        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
-
-        return ResponseEntity.status(errorCode.getStatusCode())
-                .body(ApiResponse.builder()
-                        .code(errorCode.getCode())
-                        .message(errorCode.getMessage())
-                        .build());
-    }
-
-    @ExceptionHandler(value = org.springframework.dao.DataIntegrityViolationException.class)
-    ResponseEntity<ApiResponse> handlingDataIntegrityViolationException(DataIntegrityViolationException exception) {
-        ErrorCode errorCode = ErrorCode.ERROR_SAVE_DATA;
-        return ResponseEntity.status(errorCode.getStatusCode())
-                .body(ApiResponse.builder()
-                        .code(errorCode.getCode())
-                        .message(errorCode.getMessage())
-                        .build());
-    }
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception) {
-        String enumKey = exception.getFieldError().getDefaultMessage();
-
-        ErrorCode errorCode = ErrorCode.INVALID_KEY;
-        Map<String, Object> attributes = null;
-        try {
-            errorCode = ErrorCode.valueOf(enumKey);
-
-            var constraintViolation =
-                    exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
-
-            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
-
-            log.info(attributes.toString());
-
-        } catch (IllegalArgumentException e) {
-
-        }
-
-        ApiResponse apiResponse = new ApiResponse();
-
-        apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(
-                Objects.nonNull(attributes)
-                        ? mapAttribute(errorCode.getMessage(), attributes)
-                        : errorCode.getMessage());
-
-        return ResponseEntity.badRequest().body(apiResponse);
-    }
-
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handlingI(MethodArgumentNotValidException exception) {
-    String enumKey = exception.getFieldError().getDefaultMessage();
-
-    ErrorCode errorCode = ErrorCode.INVALID_KEY;
-    Map<String, Object> attributes = null;
-    try {
-        errorCode = ErrorCode.valueOf(enumKey);
-
-        var constraintViolation =
-                exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
-
-        attributes = constraintViolation.getConstraintDescriptor().getAttributes();
-
-        log.info(attributes.toString());
-
-    } catch (IllegalArgumentException e) {
-        log.error("Invalid error code: " + enumKey, e);
-    }
-
-    ApiResponse apiResponse = new ApiResponse();
-
-    apiResponse.setCode(errorCode.getCode());
-    apiResponse.setMessage(
-        Objects.nonNull(attributes)
-            ? mapAttribute(errorCode.getMessage(), attributes)
-            : (exception.getMessage() != null
-                ? exception.getMessage() // Use the provided exception message
-                : errorCode.getMessage()) // Fallback to default message
-    );
-
-    return ResponseEntity.badRequest().body(apiResponse);
-}
 
 
     private String mapAttribute(String message, Map<String, Object> attributes) {
